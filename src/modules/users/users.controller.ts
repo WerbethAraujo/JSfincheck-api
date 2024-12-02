ee119547de3e1a2405b4d01fb32c0ff6ffe2f746
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-12-02 10:59:25 -0300

    feat: created route to search for logged in user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersServive: UsersService) {}
+
+  @Get('/me')
+  me(@Req() request: any) {
+    return this.usersServive.getUserById(request.userId);
+  }
+}
